0c4dfd1eb9a781d239ece8cd88d9d773d3e329cf
kushal-vithalani <vithalani75@gmail.com>
2024-12-07 16:45:45 +0530

    Update dbConnection.php

--- dbConnection.php
+++ dbConnection.php
@@ -1,8 +1,8 @@
 <?php
-$databaseHost = 'localhost';
-$databaseName = 'test';
-$databaseUsername = 'root';
-$databasePassword = 'root';
+$databaseHost = 'admin.ccao5tlwblcq.ap-south-1.rds.amazonaws.com';
+$databaseName = 'dbdb';
+$databaseUsername = 'admin';
+$databasePassword = 'admin123';
 
 // Open a new connection to the MySQL server
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
