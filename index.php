fcaf5f99fc02d74c63ed9815a362ccafaebbf8c5
kushal-vithalani <vithalani75@gmail.com>
2024-12-07 18:26:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC");
 <head>	
 	<title>Homepage</title>
 </head>
-	<h1>kushal</h1>
+	<h1>kushalvithani</h1>
 
 <body>
 	<h2>Homepage</h2>
