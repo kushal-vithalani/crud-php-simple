a6bae1631c61f66bd0be73e514828adfaf2970d3
kushal-vithalani <vithalani75@gmail.com>
2024-12-07 16:14:03 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
-
-# Variables
-APP_DIR="/var/www/html"
-TEMP_DIR="/tmp/app"
-
-# Create temporary directory
-mkdir -p $TEMP_DIR
-
-# Copy files
-cp -r * $TEMP_DIR
-
-# Sync files to application directory
-sudo rsync -av --delete $TEMP_DIR/ $APP_DIR/
-
-# Restart Apache
-sudo systemctl restart httpd
-
-# Cleanup
-rm -rf $TEMP_DIR
+echo "Starting deployment..."
+cp -r * /var/www/html/
+echo "Deployment successful!"
